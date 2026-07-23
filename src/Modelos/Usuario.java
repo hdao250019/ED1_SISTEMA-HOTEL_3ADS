93ff25e5490d64326a5ff5cd9c5e729447f33a12
Angel H <Angel H@172.23.51.124>
2026-07-23 08:35:43 -0600

    Conexion

--- src/Modelos/Usuario.java
+++ src/Modelos/Usuario.java
@@ -9,5 +9,32 @@ package Modelos;
  * @author Angel H
  */
 public class Usuario {
+    private String nombre, mail, pass, rol;
+
+    public Usuario(String nombre, String mail, String pass, String rol) {
+        this.nombre = nombre;
+        this.mail = mail;
+        this.pass = pass;
+        this.rol = rol;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.mail = mail;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
     
+    public boolean validarLogin(String correoLogin, String passLogin){
+        return mail.equals(correoLogin) && pass.equals(passLogin);
+    }
 }
