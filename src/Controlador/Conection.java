ffc8a5ba18e24780a8c7aea90ce922f02383b5b7
Angel H <Angel H@172.23.51.124>
2026-07-23 08:33:54 -0600

    Conexion

--- src/Controlador/Conection.java
+++ src/Controlador/Conection.java
@@ -3,11 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Controlador;
-
+import java.sql.*;
 /**
  *
  * @author Angel H
  */
 public class Conection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ed1_sistemahotel";
+    private static final String USER = "root";
+    private static final String PASS = "";
     
+    //Metodo para realizar la conexion
+    public static Connection conexion(){
+        Connection conn = null;
+        // Manejar el error en la conexion con la base de datos
+        try{
+            //Guardar conexion en el objeto connection
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Conexion exitosa");
+            
+        }catch(SQLException e){
+            //Manejar el error de SQL
+            System.out.print("Error en la conexion: "+e.getMessage());
+        }
+        
+        return conn;
+    }
 }
